d1451028461c65f4e1a083fe7e5f421ae642037d
1x000 <huaweilaomke402@gmail.com>
2023-07-27 22:43:31 +0700

    2023/07/27 周四 22:43:14.80

---
+++ phpinfo.php
@@ -0,0 +1,47 @@
+<?php phpinfo();?>
+
+PHP Version 6.6.6
+
+System	Linux host208.bbsn.co.jp 2.6.9-103.ELsmp #1 SMP Fri Dec 9 04:31:51 EST 2011 i686
+Build Date	Feb 2 2222 18:55:00
+
+allow_url_fopen = On
+allow_url_include = On
+
+
+你们脚本小子整天扫网站有意思吗？扫之前不做个指纹识别一下？AWVS 扫静态网站是不是有病，能扫出个鬼啊，静态网站连后台都没有，真不知道扫啥玩意儿
+另外网站有日志的知道吗，AWVS 特征这么明显，你服务器高匿名了吗？非法扫描入侵违法的知道吗？真要扫搞个多层高匿服务器再来吧，虽然最后还是扫不出啥玩意。
+
+
+您已经发现我们的网站,这说明您掌握了一定的技能。
+但请记住,仅靠扫描器是不够的。
+若想成为优秀的安全研究人员,还需要学习:
+
+- Web应用安全
+- 后端代码安全
+- 网络协议安全
+- 系统漏洞分析
+- ...
+
+我建议您关注这些主题,再回来与我们交流。
+互利共赢,共建一个更安全的网络。
+
+
+You have discovered our website, which means you have a certain skill.
+But remember, scanners alone are not enough.
+If you want to become an excellent security researcher, you also need to learn:
+
+
+- Web application security
+- Backend code security
+- Internet protocol security
+- System vulnerability analysis
+-...
+
+
+I encourage you to follow these topics and come back to chat with us.
+Mutual benefit and win-win, to build a more secure network.
+
+
+Do you script kiddies have fun scanning websites all day? Don't do a fingerprint recognition before scanning? AWVS scan static website is not sick, can scan a ghost, static website does not even have a background, I really don't know what to scan
+In addition, do you know that the website has logs? The AWVS feature is so obvious. Is your server highly anonymous? Do you know that illegal scanning is illegal? I really want to sweep up a multi-layer high-secret server and come back, although I still can't find anything in the end.
